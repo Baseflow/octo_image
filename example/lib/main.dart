ef016a61168d8f98f88d69bc5bd8f42be0e6d145
Rene Floor <r.floor.1@gmail.com>
2020-04-30 12:49:16 +0200

    Added CachedNetworkImage in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:octo_image/octo_image.dart';
 
@@ -36,9 +37,10 @@ class OctoImagePage extends StatelessWidget {
     return AspectRatio(
       aspectRatio: 269 / 173,
       child: OctoImage(
-        image: NetworkImage('https://blurha.sh/assets/images/img1.jpg'),
-        placeholderBuilder:
-            OctoPlaceholder.blurHash('LEHV6nWB2yk8pyo0adR*.7kCMdnj'),
+        image: CachedNetworkImageProvider('https://blurha.sh/assets/images/img1.jpg'),
+        placeholderBuilder: OctoPlaceholder.blurHash(
+          'LEHV6nWB2yk8pyo0adR*.7kCMdnj',
+        ),
         errorBuilder: OctoError.icon(color: Colors.red),
         fit: BoxFit.cover,
       ),
@@ -50,8 +52,9 @@ class OctoImagePage extends StatelessWidget {
       height: 160,
       child: OctoImage.fromSet(
           fit: BoxFit.cover,
-          image: NetworkImage(
-              'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Macaca_nigra_self-portrait_large.jpg/1024px-Macaca_nigra_self-portrait_large.jpg'),
+          image: CachedNetworkImageProvider(
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Macaca_nigra_self-portrait_large.jpg/1024px-Macaca_nigra_self-portrait_large.jpg',
+          ),
           octoSet: OctoSet.circleAvatar(
               backgroundColor: Colors.red, text: Text("T"))),
     );
--- test/octo_image_test.dart
+++ test/octo_image_test.dart
@@ -2,13 +2,12 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:octo_image/octo_image.dart';
-import 'package:octo_image/src/octo_set.dart';
 
 void main() {
   test('adds one to input values', () {
-    OctoImage.fromSet(
+    OctoImage(
       image: NetworkImage('https://dummyimage.com/600x400/000/fff'),
-      octoSet: OctoSet.simple(),
+      imageBuilder: OctoImageTransformer.circleAvatar(),
     );
   });
 }
